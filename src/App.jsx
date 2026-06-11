ae714406a2135c025c98c698f71cb9bf6d17ea25
Tilly-py <alex.s.tilly@outlook.com>
2026-06-11 10:48:14 +0200

    Added tilted card component and collection page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from '../src/components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import OpenPackPage from './pages/OpenPackPage';
 const app = () => {
   return (
     <div className="min-h-screen bg-zinc-950">
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/open" element={<OpenPackPage image="./src/assets/pokemon-card-back.png" />} />
       </Routes>
     </div>
   );
