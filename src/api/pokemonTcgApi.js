422dfec396b80439a1758c21cb6d55065ae0f47f
Tilly-py <alex.s.tilly@outlook.com>
2026-06-11 19:50:46 +0200

    Added API functionality to fetch base set cards inside 'openPackPage' component we handle the api call and error handling

---
+++ src/api/pokemonTcgApi.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'https://api.pokemontcg.io/v2';
+
+export const getCardsBySet = async (setId) => {
+    const response = await fetch(
+        `${BASE_URL}/cards?q=set.id:${setId}&pageSize=250`
+    );
+    if (!response.ok) {
+        throw new Error('Failed to fetch cards for the specified set.');
+    }
+    const data = await response.json();
+    return data;
+};
--- src/pages/OpenPackPage.jsx
+++ src/pages/OpenPackPage.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react';
 import TiltCard from '../components/TiltCard/TiltCard';
+import { getCardsBySet } from '../api/pokemonTcgApi';
 
 const OpenPackPage = () => {
   const demoCardImage = 'https://images.pokemontcg.io/base1/1_hires.png';
+  const [cards, setCards] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
 
+  useEffect(() => {
+    const loadCards = async () => {
+      try {
+        setIsLoading(true);
+        setErrorMessage('');
+
+        const baseSetCards = await getCardsBySet('base1');
+        console.log(baseSetCards);
+        setCards(baseSetCards);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadCards();
+  }, []);
+  const previewCard = cards[0];
   return (
     <section className="mx-auto flex min-h-screen w-full max-w-4xl flex-col items-center justify-center gap-6 text-center">
       <p className="mb-3 text-sm font-semibold tracking-[0.3em] text-yellow-400">
@@ -14,9 +38,16 @@ const OpenPackPage = () => {
       <p className="mt-6 max-w-2xl text-lg text-gray-400">
         Reveal cards one by one, and sleeve your favorites to keep them safe!
       </p>
-      <div className="mt-10">
-        <TiltCard image={demoCardImage} title="Alakazam" />
+      <div className="mt-6 rounded-2xl border border-zinc-800 bg-zinc-900 px-5 py-4 text-sm text-zinc-300">
+        {isLoading && <p>Loading...</p>}
+
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
       </div>
+      {previewCard && (
+        <div className="mt-10">
+          <TiltCard image={previewCard.images.large} title={previewCard.name} />
+        </div>
+      )}
     </section>
   );
 };
