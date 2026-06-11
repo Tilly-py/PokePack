15ecebe48d527435589a19d72a3161bfdb3b9b6b
Tilly-py <alex.s.tilly@outlook.com>
2026-06-11 09:36:14 +0200

    Added HomePage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../src/components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 const app = () => {
   return (
     <div className="min-h-screen bg-zinc-950">
       <Navbar />
-      <h1 className="text-3xl font-bold bg-zinc-800 text-white p-4">PokePack</h1>
-      <p className="text-lg text-gray-700 p-4">Welcome to PokePack, gotta catch 'em all!</p>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 };
