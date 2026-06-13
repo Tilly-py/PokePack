cb592636c06ecb3946f1444e658c063f0398bdba
Tilly-py <alex.s.tilly@outlook.com>
2026-06-13 10:12:01 +0200

    Removed some dead styling for the loader inside of index.css

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -8,8 +8,8 @@ const LoadingScreen = () => {
           bg-[length:600%_600%]
           [animation:pokeLoading_10s_linear_infinite]"
       >
-        <div className=" worm absolute h-[105px] w-[10px] rounded-md bg-white [animation:pokeShadow_4s_linear_infinite]" />
-        <div className=" circleMiddle relative z-10 h-[80px] w-[80px] rounded-full bg-zinc-900" />
+        <div className="absolute h-[105px] w-[20px] rounded-md bg-white [animation:pokeShadow_4s_linear_infinite]" />
+        <div className="relative z-10 h-[80px] w-[80px] rounded-full bg-zinc-900" />
       </div>
       <div className="text-center">
         <p className="text-sm font-bold uppercase tracking-[0.25em] text-yellow-400">Loading</p>
